a9f1266a2811f32b795d7241fd7e4e8aa03f4e36
2kmehdi <homehaventips@gmail.com>
2026-01-13 22:48:12 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import AppleTVPlayer from './components/AppleTVPlayer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppleTVPlayer />
+    </div>
+  );
+}
+
+export default App;
